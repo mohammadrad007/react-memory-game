912bd268c9ce2e8e6f895893afdd2fe8ba41c2e2
mohammad mohammadi <mohammad.mohamadi0101@gmail.com>
2022-04-18 20:24:37 +0430

    react router dom and configs

--- src/App.js
+++ src/App.js
@@ -1,83 +1,26 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SingleCard from "./components/SingleCard";
 import "./App.css";
-
-const cardImages = [
-  { src: "/img/helmet-1.png", matched: false },
-  { src: "/img/potion-1.png", matched: false },
-  { src: "/img/ring-1.png", matched: false },
-  { src: "/img/scroll-1.png", matched: false },
-  { src: "/img/shield-1.png", matched: false },
-  { src: "/img/sword-1.png", matched: false },
-];
+import LevelOne from "./components/levelOne/LevelOne";
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disabled, setDisabled] = useState(false);
-
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
-
-    setCards(shuffledCards);
-    setTurns(0);
-  };
-
-  const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-  };
-
-  const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setTurns((prevTurn) => prevTurn + 1);
-    setDisabled(false);
-  };
-
-  useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      setDisabled(true);
-      if (choiceOne.src === choiceTwo.src) {
-        setCards((prevCards) => {
-          return prevCards.map((card) => {
-            if (card.src === choiceOne.src) {
-              return { ...card, matched: true };
-            } else {
-              return card;
-            }
-          });
-        });
-        resetTurn();
-      } else {
-        setTimeout(() => {
-          resetTurn();
-        }, 1000);
-      }
-    }
-  }, [choiceOne, choiceTwo]);
-
   return (
-    <div className="App">
-      <h1>React Memory Match</h1>
-      <button onClick={shuffleCards}>New Game</button>
-
-      <p>{turns}</p>
-      <div className="card-grid">
-        {cards.map((card) => (
-          <SingleCard
-            card={card}
-            handleChoice={handleChoice}
-            key={card.id}
-            flipped={card === choiceOne || card === choiceTwo || card.matched}
-            disabled={disabled}
-          />
-        ))}
+    <BrowserRouter>
+      <div className="App">
+        <p>Welcome to React Memory App (RMA)</p>
+        <span>if you are ready, then let start game</span>
+        <Link to="levelone">go to game</Link>
       </div>
-    </div>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/levelone" element={<LevelOne />}></Route>
+        {/* <Route path="/users">
+            <Users />
+          </Route>
+        */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/levelOne/LevelOne.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import SingleCard from "../SingleCard";
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
+const LevelOne = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurn) => prevTurn + 1);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => {
+          resetTurn();
+        }, 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+  return (
+    <div>
+      <h1>React Memory Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <p>{turns}</p>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            card={card}
+            handleChoice={handleChoice}
+            key={card.id}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LevelOne;
