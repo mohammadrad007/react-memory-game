bff6458583466caf074b18fe882c88492449b7c5
mohammad mohammadi <mohammad.mohamadi0101@gmail.com>
2022-04-18 11:21:27 +0430

    add fonts, images and ...

--- src/App.js
+++ src/App.js
@@ -3,19 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Memory Match</h1>
+      <button>New Game</button>
     </div>
   );
 }
