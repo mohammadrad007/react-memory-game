fa084477542c58f465681c65b1dec5e98ef9ae54
mohammad mohammadi <mohammad.mohamadi0101@gmail.com>
2022-04-18 12:03:29 +0430

    develope single card components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import SingleCard from "./components/SingleCard";
 import "./App.css";
 
 const cardImages = [
@@ -30,12 +31,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="card front" />
-              <img className="back" src="/img/cover.png" alt="card back" />
-            </div>
-          </div>
+          <SingleCard card={card} key={card.id} />
         ))}
       </div>
     </div>
