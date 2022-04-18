36343170c67c238ffc49d2600c3d7c7753d31b09
mohammad mohammadi <mohammad.mohamadi0101@gmail.com>
2022-04-18 11:46:52 +0430

    shuffed images array and give them unique ids

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/helmetring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  console.log(cards);
   return (
     <div className="App">
       <h1>React Memory Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
